5bfd6ad7bb627ec3ca159705a9b410f978ad287e
jir999 <jir.avetisyan@gmail.com>
2020-06-05 23:39:58 +0400

    Fetch API Error Processing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const getResource = async (url) => {
     const res = await fetch(url);
+    
+    if(!res.ok){
+        throw new Error(`${url, res.status}`)
+    }
+
     const body = await res.json();
     return body;
 };
 
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/1/123')
     .then((body) => {
         console.log(body);
+    })
+    .catch((err) => {
+        console.error(err)
     })
 
 // fetch("https://swapi.dev/api/people/1/")
