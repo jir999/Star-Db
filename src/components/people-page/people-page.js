ad216660ec3f20f9190c6f62e3b6aa54f9d272a7
jir999 <jir.avetisyan@gmail.com>
2020-06-20 14:01:35 +0400

    Render Function (React Pattern)

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import ErrorIndicator from "../error-indicator";
+import SwapiService from "../../services/swapi-service";
 
 import './people-page.css';
 
 export default class PeoplePage extends Component {
+
+    swapiService = new SwapiService();
 
     state = {
         selectedPerson: 2,
@@ -35,7 +38,9 @@ export default class PeoplePage extends Component {
         return(
             <div className="row mb2">
                 <div className="col-md-6">
-                    <ItemList onItemSelected={this.onPersonSelected} />
+                    <ItemList onItemSelected={this.onPersonSelected} 
+                              getData={this.swapiService.getAllPeople}
+                              renderItem={({ name, gender, birthYear}) => `${name} (${gender}, ${birthYear})` } />
                 </div>
                 <div className="col-md-6">
                     <PersonDetails personId={this.state.selectedPerson} />
